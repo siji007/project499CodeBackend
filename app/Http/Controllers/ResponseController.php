73f3b5bb844fc224a9f5fcc39361975489f48df9
siji007 <pomolia007@gmaile.com>
2021-12-06 23:46:42 +0100

    add store and index methods

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -15,7 +15,8 @@ class ResponseController extends Controller
      */
     public function index()
     {
-        //
+        $responses = Response::all();
+        return response($responses);
     }
 
     /**
@@ -36,7 +37,42 @@ class ResponseController extends Controller
      */
     public function store(StoreResponseRequest $request)
     {
-        // Q;
+        // $Response = Response::class;
+        $questionAnswer = [
+            Response::$question1 => $request->question1,
+            Response::$question2 => $request->question2,
+            Response::$question3 => $request->question3,
+            Response::$question4 => $request->question4,
+            Response::$question5 => $request->question5,
+            Response::$question6 => $request->question6,
+            Response::$question7 => $request->question7,
+            Response::$question8 => $request->question8,
+            Response::$question9 => $request->question9,
+            Response::$question10 => $request->question10,
+            Response::$question11 => $request->question11,
+            Response::$question12 => $request->question12,
+            Response::$question13 => $request->question13,
+            Response::$question15 => $request->question15,
+            Response::$question16 => $request->question16,
+            Response::$question17 => $request->question17,
+            Response::$question18 => $request->question18,
+            Response::$question19 => $request->question19,
+            Response::$question20 => $request->question20,
+            Response::$question21 => $request->question21,
+            Response::$question22 => $request->question22,
+            Response::$question23 => $request->question23,
+            Response::$question24 => $request->question24,
+            Response::$question25 => $request->question25,
+        ];
+
+        // dd(json_encode($questionAnswer));
+
+        Response::create([
+            'email' => $request->email,
+            'question_answer' => json_encode($questionAnswer)
+        ]);
+
+        return response('Response recorded! Thanks');
     }
 
     /**
