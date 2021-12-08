f808b2f0711426538dfba35231defb39dccbf794
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-12-08 11:49:29 +0100

    add guarded array

--- app/Models/Response.php
+++ app/Models/Response.php
@@ -9,35 +9,40 @@ class Response extends Model
 {
     use HasFactory;
 
-    static $question1 = "Age";
-    static $question2 = "Gender";
-    static $question3 = "Class";
-    static $question4 = "Do you have access to information technology?";
-    static $question5 = "If Yes, what method accessing information technology?";
-    static $question6 = "Get distracted in class when they bring their phones to class";
-    static $question7 = "Get distracted by information technology media after school";
-    static $question8 = "Visit game centers or play games more than reading their books";
-    static $question9 = "Are more active on social media than their academics";
-    static $question10 = "Are exposed to pornographic information which affects their academics negatively";
-    static $question11 = "Are addicted to television after schools hours";
-    static $question12 = "Use mobile phones for examination malpractice";
-    static $question13 = "Are distracted by other mobile or computer features when attending online classes";
-    static $question14 = "Are addicted to movies or shows on their devices";
-    static $question15 = "Are distracted by listening to music in class";
-    static $question16 = "Use the internet to download unnecessary information";
-    static $question17 = "Are exposed to moral corruption through the internet";
-    static $question18 = "Are exposed to gambling through technology and affects their education";
-    static $question19 = "Are exposed to internet scam which affects their education";
-    static $question20 = "Have become lazy in their academics since they get most information online";
-    static $question21 = "Are misguided academically due to wrong information online";
-    static $question22 = "Lose the habit to write due to excessive use of laptops and mobile phones";
-    static $question23 = "Are more likely to loose their data on laptops or mobile phones than having physical books";
-    static $question24 = "Are more serious in attending physical classes than online classes";
-    static $question25 = "Are you motivated to read physical books than reading from electronic devices";
+    // static $question1 = "Age";
+    // static $question2 = "Gender";
+    // static $question3 = "Class";
+    // static $question4 = "Do you have access to information technology?";
+    // static $question5 = "If Yes, what method accessing information technology?";
+    // static $question6 = "Get distracted in class when they bring their phones to class";
+    // static $question7 = "Get distracted by information technology media after school";
+    // static $question8 = "Visit game centers or play games more than reading their books";
+    // static $question9 = "Are more active on social media than their academics";
+    // static $question10 = "Are exposed to pornographic information which affects their academics negatively";
+    // static $question11 = "Are addicted to television after schools hours";
+    // static $question12 = "Use mobile phones for examination malpractice";
+    // static $question13 = "Are distracted by other mobile or computer features when attending online classes";
+    // static $question14 = "Are addicted to movies or shows on their devices";
+    // static $question15 = "Are distracted by listening to music in class";
+    // static $question16 = "Use the internet to download unnecessary information";
+    // static $question17 = "Are exposed to moral corruption through the internet";
+    // static $question18 = "Are exposed to gambling through technology and affects their education";
+    // static $question19 = "Are exposed to internet scam which affects their education";
+    // static $question20 = "Have become lazy in their academics since they get most information online";
+    // static $question21 = "Are misguided academically due to wrong information online";
+    // static $question22 = "Lose the habit to write due to excessive use of laptops and mobile phones";
+    // static $question23 = "Are more likely to loose their data on laptops or mobile phones than having physical books";
+    // static $question24 = "Are more serious in attending physical classes than online classes";
+    // static $question25 = "Are you motivated to read physical books than reading from electronic devices";
 
     protected $casts = [
         'question_answer' => 'array'
     ];
-
-    protected $fillable = ['email', 'question_answer'];
+    
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $guarded = ['created_at', 'updated_at', 'id'];
 }
